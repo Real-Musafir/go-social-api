cdd5a5ee509c25759ac052b2015436c45d2db1e1
alshahadath <abdullah.alshahadath@vstinc.com>
2024-11-02 20:44:18 +0600

    server is working with CHI

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -3,6 +3,10 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 type application struct {
@@ -10,15 +14,30 @@ type application struct {
 }
 
 type config struct {
-	addr string
+	addr string 
 }
 
-func (app *application) run() error {
-	mux := http.NewServeMux()
+
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.healthCheckHandler)
+	})
+	return r
+}
+
+
+func (app *application) run(mux http.Handler) error {
 
 	srv := &http.Server{
 		Addr : app.config.addr,
 		Handler: mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout: time.Second * 10,
+		IdleTimeout: time.Minute,
 	}
 
 	log.Printf("Server has started at %s", app.config.addr)
---
+++ cmd/api/health.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request){
+
+	fmt.Print("Come here to run")
+	w.Write([]byte("ok"))
+}
