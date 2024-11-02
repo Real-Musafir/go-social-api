6cca887740e03cf43b830550f9ce434af0f82aaf
alshahadath <abdullah.alshahadath@vstinc.com>
2024-11-02 22:51:17 +0600

    ❤️ Setup env variable ❤️

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,15 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	env "github.com/Real-Musafir/social/internal"
+)
 
 func main() {
 
 	cfg := config {
-		addr: ":8080",
+		addr: env.GetString("ADDR", ":8080"),
 	}
 
 	app := &application{
