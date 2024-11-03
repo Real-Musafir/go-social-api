f55d56c501c87db1892526769bc98d87487b3835
alshahadath <abdullah.alshahadath@vstinc.com>
2024-11-03 15:33:39 +0600

    ❤️❤️ database connected successfully ❤️❤️

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -13,7 +13,7 @@ func main() {
 	cfg := config {
 		addr: env.GetString("ADDR", ":8080"),
 		db: dbConfig{
-			addr: env.GetString("DB_ADDR", "postgres://shahadath@localhost/social?sslmode=disable"),
+			addr: env.GetString("DB_ADDR", "postgres://shahadath@localhost/socialnetwork?sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
 			maxIdleConnes: env.GetInt("DB_MAX_IDLE_CONNS", 30),
 			maxIdleTime: env.GetString("DB_MAX_IDLE_TIME", "15m"),
