aa48ef4e0df52b57c8906dac2f1e03090ef8871d
alshahadath <abdullah.alshahadath@vstinc.com>
2024-11-03 14:30:30 +0600

    WIP db integration

--- internal/store/users.go
+++ internal/store/users.go
@@ -10,6 +10,35 @@ type UsersStore struct {
 	db *sql.DB
 }
 
-func (s *UsersStore) Create(ctx context.Context) error {
+type User struct {
+	ID 			int16		`json:"id"`
+	Username 	string		`json:"username"`
+	Email 		string		`json:"email"`
+	Password 	int16		`json:"password"`
+	CreatedAt 	string		`json:"created_at"`
+}
+
+func (s *UsersStore) Create(ctx context.Context, user *User) error {
+
+	query := `
+				INSERT INTO posts (username, password, email)
+				VALUES ($1, $2, $3) RETURNING id, created_at
+			`
+
+	err := s.db.QueryRowContext(
+		ctx,
+		query,
+		user.Username,
+		user.Password,
+		user.Email,
+	).Scan(
+		&user.ID,
+		&user.CreatedAt,
+	)
+
+	if err!= nil {
+		return err
+	}
+			
 	return nil
 }
