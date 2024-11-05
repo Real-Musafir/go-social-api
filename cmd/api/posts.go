57dd67b3cee881f12a1db75058bea87363b19ccf
alshahadath <abdullah.alshahadath@vstinc.com>
2024-11-05 23:05:27 +0600

    clean the error response

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -20,12 +20,9 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 	var payload CreatePostPayload
 
 	if err := readJSON(w, r, &payload); err != nil {
-		writeJSONError(w, http.StatusBadRequest, err.Error())
+		app.badRequestResponse(w, r, err)
 		return
 	}
-
-
-	
 
 	post := &store.Post {
 		Title: payload.Title,
@@ -37,12 +34,12 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 	ctx := r.Context()
 
 	if err:= app.store.Posts.Create(ctx, post); err != nil {
-		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		app.internalServerError(w, r, err)
 		return
 	}
 
 	if err := writeJSON(w, http.StatusCreated, post); err != nil {
-		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		app.internalServerError(w, r, err)
 		return
 	}
 }
@@ -51,7 +48,7 @@ func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request){
 	idParam := chi.URLParam(r, "postID")
 	id, err := strconv.ParseInt(idParam, 10, 64)
 	if err!= nil {
-		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		app.internalServerError(w, r, err)
 		return 
 	}
 	ctx := r.Context()
@@ -61,9 +58,9 @@ func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request){
 	if err!= nil {
 		switch  {
 		case errors.Is(err, store.ErrNotFound):
-			writeJSONError(w, http.StatusNotFound, err.Error())
+			app.notFoundResponse(w, r, err)
 		default:
-			writeJSONError(w, http.StatusInternalServerError, err.Error())
+			app.internalServerError(w, r, err)
 		}
 
 		return
@@ -71,7 +68,7 @@ func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request){
 
 
 	if err := writeJSON(w, http.StatusOK, post); err != nil {
-		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		app.internalServerError(w, r, err)
 		return
 	}
 }
